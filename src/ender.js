7761baad38e5b4d4fb294f102a93aedf06ed2937
Jacob Thornton <jacobthornton@gmail.com>
2011-04-09 22:21:29 -0700

    update ender integration, give it jquery like api

--- src/ender.js
+++ src/ender.js
@@ -1,1 +1,18 @@
-$.ender(bean);
+!function () {
+  var b = bean.noConflict(),
+      integrate = function (type) {
+        return function () {
+          for (var i = 0, l = this.elements.length; i < l; i++) {
+            b[type].apply(this, [this.elements[i]].concat(Array.prototype.slice.call(arguments, 0)));
+          }
+          return this;
+        }
+      };
+
+  $.ender({
+    bind: integrate('add'),
+    unbind: integrate('remove'),
+    trigger: integrate('fire'),
+    cloneEvents: integrate('clone')
+  }, true);
+}();
