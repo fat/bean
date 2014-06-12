5da7de22302a9f6607cf55d69a3bfee6b7d17596
Piotr Deszynski <deszynski@red-sky.pl>
2014-06-12 15:56:23 +0200

    Remove unnecessary async read/write

--- make/component.js
+++ make/component.js
@@ -1,24 +1,13 @@
 var fs = require("fs");
-fs.readFile('./package.json', "utf8", function (err, data) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    var packagejson = JSON.parse(data);
-    fs.writeFile("component.json", JSON.stringify({
-            name: packagejson.name,
-            description: packagejson.description,
-            version: packagejson.version,
-            keywords: packagejson.keywords,
-            main: "bean.js",
-            scripts: ["bean.js"],
-            repo: "https://github.com/fat/bean"
-        }, null, 2),
-        function (err) {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-            }
-        }
-    );
-});
+var data = fs.readFileSync('package.json', "utf8");
+var packagejson = JSON.parse(data);
+fs.writeFileSync("component.json", JSON.stringify({
+        name: packagejson.name,
+        description: packagejson.description,
+        version: packagejson.version,
+        keywords: packagejson.keywords,
+        main: "bean.js",
+        scripts: ["bean.js"],
+        repo: "https://github.com/fat/bean"
+    }, null, 2)
+);
